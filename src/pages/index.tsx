d8a76c75fee4c5da0a65d9cd96da5e6972d0145c
Bjørn Olav Gjørven <bjorn.gjorven@iterate.no>
2023-03-07 20:16:14 +0100

    Add random comment for deploy test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import { signIn, signOut, useSession } from "next-auth/react";
 
 import { api } from "~/utils/api";
 
+// Main page component
 const Home: NextPage = () => {
   const hello = api.example.hello.useQuery({ text: "from tRPC" });
 
