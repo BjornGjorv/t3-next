4445f34e03695ee1589545c5c1c722bc4fe0b535
Bjørn Olav Gjørven <bjorn.gjorven@iterate.no>
2023-03-08 11:19:07 +0100

    Chore: Install headless ui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,8 @@ import { type NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
 import { signIn, signOut, useSession } from "next-auth/react";
-
 import { api } from "~/utils/api";
 
-// Main page component
 const Home: NextPage = () => {
   const hello = api.example.hello.useQuery({ text: "from tRPC" });
 
