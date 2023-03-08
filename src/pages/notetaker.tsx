64aff3262e981d7c5b7bb39e190f5f9ca1d1c3c5
Bjørn Olav Gjørven <bjorn.gjorven@iterate.no>
2023-03-08 15:20:02 +0100

    Fetch and show notes from the database oOooOOo

--- src/pages/notetaker.tsx
+++ src/pages/notetaker.tsx
@@ -1,8 +1,10 @@
+import { useState } from "react";
 import { type NextPage } from "next";
 import Head from "next/head";
 import NavBar from "~/components/NavBar";
 import { useSession } from "next-auth/react";
-import { api } from "~/utils/api";
+// import { api } from "~/utils/api";
+import { api, type RouterOutputs } from "../utils/api";
 
 const Home: NextPage = () => {
 
@@ -27,8 +29,11 @@ const Home: NextPage = () => {
 
 export default Home;
 
+type Topic = RouterOutputs["topic"]["getAll"][0];
+
 const Content: React.FC = () => {
   const { data: sessionData } = useSession();
+  const [selectedTopic, setSelectedTopic] = useState<Topic | null>(null)
 
   const {data: topics, refetch: refetchTopics} = api.topic.getAll.useQuery(
     undefined, 
@@ -37,29 +42,49 @@ const Content: React.FC = () => {
       }
     );
 
-    const createTopic = api.topic.create.useMutation({});
+    const createTopic = api.topic.create.useMutation({
+      onSuccess: () => {
+        void refetchTopics();
+      }
+    });
 
     // return <div>{JSON.stringify(topics)}</div>
     return (
-      <div className="mx-5 mt-5 grid grid-cols-4 gap-2">
-            <div className="px-2">
-            <div className="col-span-3">
-            <div className="divider"></div>
-                <input
-                  type="text"
-                  placeholder="New Topic"
-                  className="input-bordered input input-sm w-full"
-                  onKeyDown={(e) => {
-                    if (e.key === "Enter") {
-                      createTopic.mutate({
-                        title: e.currentTarget.value,
-                      });
-                      e.currentTarget.value = "";
-                    }
-                  }}
-                />
-              </div>
+      // <div className="mx-5 mt-5 grid grid-cols-4 gap-2">
+      <div>
+          <div className="px-2">
+          <div className="col-span-3">
+          <ul className="menu rounded-box w-56 bg-base-100 p-2">
+          {topics?.map((topic) => (
+            <li key={topic.id}>
+              <a
+                href="#"
+                onClick={(evt) => {
+                  evt.preventDefault();
+                  setSelectedTopic(topic);
+                }}
+              >
+                {topic.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+          <div className="divider"></div>
+              <input
+                type="text"
+                placeholder="New Topic"
+                className="input-bordered input input-sm w-full"
+                onKeyDown={(e) => {
+                  if (e.key === "Enter") {
+                    createTopic.mutate({
+                      title: e.currentTarget.value,
+                    });
+                    e.currentTarget.value = "";
+                  }
+                }}
+              />
             </div>
+          </div>
       </div>
     )
     
