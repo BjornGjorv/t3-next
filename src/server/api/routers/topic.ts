e91aa1a3729116835a6baca8d6bdbe38fa549d72
Bjørn Olav Gjørven <bjorn.gjorven@iterate.no>
2023-03-08 14:15:14 +0100

    Copy a bunch of code from the tutorial and break stuff

--- src/server/api/routers/topic.ts
+++ src/server/api/routers/topic.ts
@@ -1,17 +1,42 @@
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-import { z }  from 'zod';
+// /* eslint-disable @typescript-eslint/no-unsafe-call */
+// import { z }  from 'zod';
 
-import { createTRPCRouter, protectedProcedure } from '../trpc';
+// import { createTRPCRouter, protectedProcedure } from '../trpc';
+
+// export const topicRouter = createTRPCRouter({
+//     getAll: protectedProcedure.query(({ ctx }) => {
+//         // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-member-access
+//         return ctx.prisma.topic.findMany({
+//             where: {userId: ctx.session.user.id},
+//         });
+//     }),
+//     create: protectedProcedure.input(z.object({title: z.string()})).mutation(({ctx, input}) => {
+//         // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-member-access
+//         return ctx.prisma.topic.create({data: {title: input.title, userId: ctx.session.user.id}})
+//     })
+//   });
+
+import { z } from "zod";
+
+import { createTRPCRouter, protectedProcedure } from "../trpc";
 
 export const topicRouter = createTRPCRouter({
-    getAll: protectedProcedure.query(({ ctx }) => {
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-member-access
-        return ctx.prisma.topic.findMany({
-            where: {userId: ctx.session.user.id},
-        });
+  getAll: protectedProcedure.query(({ ctx }) => {
+    return ctx.prisma.topic.findMany({
+      where: {
+        userId: ctx.session.user.id,
+      },
+    });
+  }),
+
+  create: protectedProcedure
+    .input(z.object({ title: z.string() }))
+    .mutation(({ ctx, input }) => {
+      return ctx.prisma.topic.create({
+        data: {
+          title: input.title,
+          userId: ctx.session.user.id,
+        },
+      });
     }),
-    create: protectedProcedure.input(z.object({title: z.string()})).mutation(({ctx, input}) => {
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-member-access
-        return ctx.prisma.topic.create({data: {title: input.title, userId: ctx.session.user.id}})
-    })
-  });
+});
