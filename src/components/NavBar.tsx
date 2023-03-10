b9f38331f0c47f1165d22d1ccb75bf4ec869e9ff
Bjørn Olav Gjørven <bjorn.gjorven@iterate.no>
2023-03-10 16:14:19 +0100

    Add placeholder name and logo to navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,17 +1,19 @@
 import { signIn, useSession } from "next-auth/react";
 import Link from "next/link";
+import LogoSVG from "~/icons/logoSVG";
 import DropDownMenu from "./DropDownMenu";
 
 const NavBar = () => {
   const { data: sessionData } = useSession();
 
   return (
-    <nav className="navbar bg-base-100">
-      <section className="flex-1 p-5 pl-5 text-lg font-bold">
-        <Link href="/">
-          {sessionData?.user?.name
-            ? `Notes for ${sessionData.user.name}`
-            : "Something went wrong :("}
+    <nav className="navbar bg-base-100 p-7">
+      <section className="flex-1 font-bold">
+        <div className="mr-3 h-16 w-16 pt-2">
+          <LogoSVG />
+        </div>
+        <Link href="/" className="text-focus text-4xl">
+          byteBrew
         </Link>
       </section>
       <div className="flex-none gap-2">
